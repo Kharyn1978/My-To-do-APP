90586c034a124f3d76584c8b3a83f1b4bb784e0f
K.Kharyn Ilunga <95341639+Kharyn1978@users.noreply.github.com>
2022-06-11 15:09:23 +0200

    saving the input field

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,12 +1,23 @@
-function addTask() {
-    let popUp =  document.getElementById("addTask");
+let popUp =  document.getElementById("addTask");
 
+//function for the addTask popUp
+function addTask() {
     popUp.style.display = "block";
 }
 
+//function for the closeTask popUp
 function closeTask() {
-    let popUp =  document.getElementById("addTask");
-
     popUp.style.display = "none";
+}
+
+//function for the saving The Task
+function saveTask() {
+    let form = document.getElementById("taskForm");
+
+    let title = form.elements["title"].value;
+    let description = form.elements["description"].value;
+    let date = form.elements["date"].value;
+    
+    console.log(title);
 }
 
